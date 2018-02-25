f264ea8062e19599a8177e91e849a25f94e42e4b
wangli <wanglii@foxmail.com>
2018-02-25 20:14:07 +0800

    fix footer setion

--- footer.php
+++ footer.php
@@ -1,6 +1,18 @@
 </div>
 
+<section id="footer_ctn">
 
+<aside id="sidebar" class="blackbg">
+	<?php dynamic_sidebar( 'dpt' ); ?>
+</aside>
+
+<footer id="footer" role="contentinfo">
+
+	<?php wp_footer();  ?>
+
+</footer>
+
+</section>
 
 </body>
 </html>
